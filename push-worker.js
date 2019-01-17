4efc8768cbff14c48661b22254eed600b1755097
Aaron Goodman <aaron.goodman@urbanairship.com>
2019-01-16 16:16:47 -0800

    added push-worker and snippet (#3)

---
+++ push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1547683798.68
+importScripts('https://web-sdk.urbanairship.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://media.giphy.com/media/3o6nUTKi7nzFxsRfoY/giphy.gif',
+  defaultTitle: 'Goodman Technical Services',
+  defaultActionURL: 'https://agoodman42.github.io/',
+  appKey: 'AK_nZV_7TmqG5Ncfk7SCGA',
+  token: 'MTpBS19uWlZfN1RtcUc1TmNmazdTQ0dBOmVsZkVNdzBRMzd4X2dtVkRJaklvbE5EY1lnZDhuQmxHVmlKdk54bmw4dU0',
+  vapidPublicKey: 'BBRg2SjILUkFBjjFg0ExJ_zcBFQRGCqDjlpvgsZQkSOruKT2ax5FUTlLcR3sIKfckwguoqLYoAQALadJAY-MS5A='
+})
