2a2545599cf93590f07a6be791b533ee3726bedb
agoodman42 <aaron.goodman@urbanairship.com>
2019-08-23 16:24:13 -0700

    Simplified site for basic testing

--- push-worker.js
+++ push-worker.js
@@ -1,10 +1,10 @@
-// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1555365071.8
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1566602012.658806
 importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
 uaSetup.worker(self, {
-  defaultIcon: 'http://aux.iconspalace.com/uploads/1631862020163135210.png',
-  defaultTitle: 'GoodmanTechnicalServices',
-  defaultActionURL: 'http://github.com/agoodman42',
-  appKey: 'ZigBLPcISzqh-y-ghDLPgg',
-  token: 'MTpaaWdCTFBjSVN6cWgteS1naERMUGdnOmhGNDRiaHlPUWFqNVdTelE0cjg2Nkh1QmYtSmRSSkF6UFN1MGdmOTBGUjg',
-  vapidPublicKey: 'BORNNcHFHp79O9Hotk8O4PvUjvsP8vEU0jXOos3sQP-8Hw__6yuo-OxJetznxfWfjRHY8ip1Eh8920qJQ-aZHoM='
+  defaultIcon: 'https://vignette.wikia.nocookie.net/bojackhorseman/images/1/1f/Vincentadultmancropped.png/revision/latest?cb\u003D20190117013957',
+  defaultTitle: 'Adultman',
+  defaultActionURL: 'http://Adultman\u002DIndustries.surge.sh',
+  appKey: 'bgWeRD4nS6qP42TrMtxOng',
+  token: 'MTpiZ1dlUkQ0blM2cVA0MlRyTXR4T25nOmhUdHJrenVIODc1aVlCVGVlMWlLRU5aQy1qU3Bkd3pUZ0JsZkl0RWdZM28',
+  vapidPublicKey: 'BD4ckQoDwA4oEQMTPfPePJiyjSLHsYDDiH3wFx-7psyzERrW0nveDYmz0fxSd8MnxwSXjvrR8cAQC6JcBR_Lkyo='
 })
