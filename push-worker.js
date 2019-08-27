9378fb3555b87c90fda98a600ac08b71592359a5
agoodman42 <aaron.goodman@urbanairship.com>
2019-08-27 16:32:22 -0700

    updated website for push worker

--- push-worker.js
+++ push-worker.js
@@ -3,7 +3,7 @@ importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
 uaSetup.worker(self, {
   defaultIcon: 'https://vignette.wikia.nocookie.net/bojackhorseman/images/1/1f/Vincentadultmancropped.png/revision/latest?cb\u003D20190117013957',
   defaultTitle: 'Adultman',
-  defaultActionURL: 'http://Adultman\u002DIndustries.surge.sh',
+  defaultActionURL: 'https://agoodman42.github.io/',
   appKey: 'bgWeRD4nS6qP42TrMtxOng',
   token: 'MTpiZ1dlUkQ0blM2cVA0MlRyTXR4T25nOmhUdHJrenVIODc1aVlCVGVlMWlLRU5aQy1qU3Bkd3pUZ0JsZkl0RWdZM28',
   vapidPublicKey: 'BD4ckQoDwA4oEQMTPfPePJiyjSLHsYDDiH3wFx-7psyzERrW0nveDYmz0fxSd8MnxwSXjvrR8cAQC6JcBR_Lkyo='
